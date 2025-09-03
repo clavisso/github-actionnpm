6488886277f4bc4cfa54146bfae702d233aab45c
sachin dwivedi <sachindwivedi@sachins-Mac-mini.local>
2025-09-04 00:28:59 +0530

    fix tests

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,7 +1,7 @@
 const { add } = require('../src/calculator');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(4);
+  expect(add(1, 2)).toBe(3);
 });
 
 test('adds 2 + 2 to equal 3', () => {
